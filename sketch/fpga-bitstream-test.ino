706a8924aa301b771fafabbbba4eafe32226512c
Simon Burkhardt <13417604+mnemocron@users.noreply.github.com>
2023-12-30 16:52:19 +0100

    Update fpga-bitstream-test.ino

--- sketch/fpga-bitstream-test.ino
+++ sketch/fpga-bitstream-test.ino
@@ -1,52 +1,48 @@
 
+#include <SPI.h>
+#include <stdio.h>
+
 #define LATCH_PIN 10
-#define CLOCK_PIN 2
+#define CLOCK_PIN 7
 
 #define CLB_BYTES (10)
-#define BITS_CONFIGURED (9*2)
+#define BITS_CONFIGURED (9*1)
 #define CLB_OFFSET (8)
+#define CLOCK_HALF_PERIOD (200)
 
 char bitstream[CLB_BYTES];
+char conf_bits[BITS_CONFIGURED] = {/*reg*/15, /*LUT_A*/24,25,26,27,28,29,30,31};
 
-#include <SPI.h>
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(LATCH_PIN, LOW);
   SPI.begin();
   SPI.setBitOrder(LSBFIRST);
+  prg_bitstream();
+}
 
-  char conf_bits[BITS_CONFIGURED] = {/*reg*/15,14, /*LUT_A*/24,25,26,27,28,29,30,31, /*LUT_B*/40,41,42,43,44,45,46,47;
-  
+void loop() {
+  do_clk();
+}
+
+void prg_bitstream(){
   for(int i=0; i<BITS_CONFIGURED; i++){
       char bt = conf_bits[i] - CLB_OFFSET;
       bitstream[bt/8] |= (1 << (bt%8));
   }
-
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
   for(int i=0; i<CLB_BYTES; i++){
     SPI.transfer(bitstream[i]);
-    if(i%2)
-      digitalWrite(LED_BUILTIN, HIGH);
-    else
-      digitalWrite(LED_BUILTIN, LOW);
   }
   digitalWrite(LATCH_PIN, HIGH);
-  delay(100);
+  delay(1);
   digitalWrite(LATCH_PIN, LOW);
-  digitalWrite(LED_BUILTIN, HIGH);
-
 }
 
-void loop() {
+void do_clk(){
   digitalWrite(CLOCK_PIN, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(CLOCK_HALF_PERIOD);
   digitalWrite(CLOCK_PIN, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(CLOCK_HALF_PERIOD);
 }
