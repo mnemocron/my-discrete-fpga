eb878d70fd52d5c6519e1a251320719a5244fb4b
Simon Burkhardt <simon@simonmartin.ch>
2024-01-04 17:12:42 +0100

    verified matching bitstream between PCB and VHDL

--- vhdl/tests/4bit-counter/write_bitstream.py
+++ vhdl/tests/4bit-counter/write_bitstream.py
@@ -5,18 +5,22 @@ BITSTREAM_FILE = './bitstream.txt'
 
 set_bits = []
 
+set_bits.append(132) # SW xpoint_3 south[3] to west [3]
 set_bits.append(133) # SW xpoint_2 south[2] to west [2]
 set_bits.append(134) # SW xpoint_1 south[1] to west [1]
 set_bits.append(135) # SW xpoint_0 south[0] to west [0]
 
+set_bits.append(124) # SW en_bus_south[3]
 set_bits.append(125) # SW en_bus_south[2]
 set_bits.append(126) # SW en_bus_south[1]
 set_bits.append(127) # SW en_bus_south[0]
 
+set_bits.append(116) # SW en_bus_west[3]
 set_bits.append(117) # SW en_bus_west[2]
 set_bits.append(118) # SW en_bus_west[1]
 set_bits.append(119) # SW en_bus_west[0]
 
+set_bits.append(4) # CBv LUT D -> bus[3] enable
 set_bits.append(5) # CBv LUT C -> bus[2] enable
 set_bits.append(6) # CBv LUT B -> bus[1] enable
 set_bits.append(7) # CBv LUT A -> bus[0] enable
@@ -31,21 +35,27 @@ set_bits.append(94) # CBh presel_2 = 6
 set_bits.append(97) # CBh presel_1 = 5
 set_bits.append(99) # CBh presel_1 = 5
 
+set_bits.append(101) # CBh presel_0 = 4
+
 set_bits.append(15) # CLB LUT en reg a
 set_bits.append(14) # CLB LUT en reg b
 set_bits.append(13) # CLB LUT en reg c
+set_bits.append(12) # CLB LUT en reg d
 
 # LUT A
 for b in [24,25,26,27,28,29,30,31] :
     set_bits.append(b) #
 
 # LUT B
-#for b in [40,41,42,43,44,45,46,47] :
-for b in [39,43,37,41] :
+for b in [36,37,38,39,40,41,42,43] :
     set_bits.append(b) #
 
 # LUT C
-for b in [51,55] :
+for b in [50,51,52,53,56,57,60,61] :
+    set_bits.append(b) #
+
+# LUT D
+for b in [65,66,68,70,72,74,76,78] :
     set_bits.append(b) #
 
 with open(BITSTREAM_FILE, 'w') as f:
